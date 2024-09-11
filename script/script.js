513cc74ee74e2dcca2cc164dc4d8352d83af1a14
Pr-Alexis <125223908+Pr-Alexis@users.noreply.github.com>
2024-09-12 00:51:39 +0200

    Fixed git generation with a range slider. There is still a small gap between the grid border and the grid elements

--- script/script.js
+++ script/script.js
@@ -2,12 +2,28 @@
 
 const value = document.querySelector("#value");
 const input = document.querySelector("#gridSize");
+const grid = document.querySelector('#grid');
     
 
 /*GRID SIZE UPDATER */
 function gridSizeUpdater(){
     let gridSizeValue=gridSize.value;
-    value.textContent=gridSizeValue;
+    let width=
+        height= (100/gridSizeValue)+"%";
+    //Clearing grid and css rules
+    grid.innerHTML="";
+    value.textContent=width;
+
+    //Creating grid elements and adding them to the grid
+    for(let i=0; i<gridSizeValue**2;i++){
+        let gridElement=document.createElement('div');
+        gridElement.className='grid-element';
+        gridElement.style.width= width;
+        gridElement.style.height= height;
+        grid.appendChild(gridElement);
+    }
+    
+
 }
 
 
